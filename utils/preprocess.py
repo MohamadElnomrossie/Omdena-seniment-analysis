6ea718a58adbb757fcd5a0779068459f06ef1b15
suriya-it19 <54205777+suriya-it19@users.noreply.github.com>
2021-05-30 21:31:09 +0530

    update for already cleaned text

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,4 +1,5 @@
 import re
+from tqdm import tqdm
 
 from utils.config import config
 
@@ -12,32 +13,33 @@ def tokenizer(
     list_ = []
     maxlen = 0
     vocab = 0
-    for i in range(len(texts)):
-        maxlen = len(texts[i]) if len(texts[i]) > maxlen else maxlen
-        for x in texts[i].lower():
-            if x in punctuations:
-                texts[i] = texts[i].replace(x, " ")
+    for i in tqdm(range(len(texts)), total=len(texts)):
+            #for x in texts[i].lower():
+            #    if x in punctuations:
+            #        texts[i] = texts[i].replace(x, " ")
 
-        texts[i] = re.sub(r'<.*?>', '', texts[i])
+            texts[i] = re.sub(r'<.*?>', '', texts[i])
 
-        texts[i] = re.sub(r'\w*\d\w*', '', texts[i])
+            texts[i] = re.sub(r'\w*\d\w*', '', texts[i])
 
-        texts[i] = re.sub(r'[0-9]+', '', texts[i])
+            texts[i] = re.sub(r'[0-9]+', '', texts[i])
 
-        texts[i] = re.sub(r'\s+', ' ', texts[i]).strip()
+            texts[i] = re.sub(r'\s+', ' ', texts[i]).strip()
 
-        texts[i] = texts[i].lower()
+            #texts[i] = texts[i].lower()
 
-        texts[i] = texts[i].split(' ')
+            texts[i] = texts[i].split(' ')
 
-        texts[i] = [x for x in texts[i] if x!='']
+            texts[i] = [x for x in texts[i] if x!='']
 
-        texts[i] = [x for x in texts[i] if x not in stop_words]
-        list_.append(texts[i])
+            #texts[i] = [x for x in texts[i] if x not in stop_words]
+            list_.append(texts[i])
 
-        maxlen = len(texts[i]) if len(texts[i]) > maxlen else maxlen
-        for i in texts[i]:
-            vocab += 1
+            maxlen = len(texts[i]) if len(texts[i]) > maxlen else maxlen
+            for i in texts[i]:
+                vocab += 1
+                
+        
 
     return list_, maxlen, vocab
 
