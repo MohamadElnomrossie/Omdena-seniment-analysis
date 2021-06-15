ae5d70d9a3253fa66884757c7348d96821d9910a
suriya-it19 <54205777+suriya-it19@users.noreply.github.com>
2021-06-15 10:27:05 +0530

    added more stop words

--- utils/config.py
+++ utils/config.py
@@ -1,12 +1,16 @@
+import pickle
+with open("Datasets/stopWords.pkl", 'rb') as f:
+    stop_words = list(pickle.load(f))
+    stop_words = list(set(stop_words + ['و','في','من','بواسطة','أ','هو','و','في','سيكون','إلى','كان','كن','هو','ال','و','ما','ء','ه','س']))
 
 config = {
-    'vocab_size':1200,# 1200
-    'maxlen':150,# 150
+    'vocab_size':20000,# 1200
+    'maxlen':256,# 150
     'embedding_vector':10,
 
-    'method':'simpleRNN',
-    'stop_words':['و','في','من','بواسطة','أ','هو','و','في','سيكون','إلى','كان','كن','هو','ال','و','ما','ء','ه'],
-    'punctuations':"""'!"#$%&'()*+,«».؛،/:؟?@[\]^_`{|}~""",
+    'method':'lstm',
+    'stop_words':stop_words,
+    'punctuations':"""'!"-#$%&'()*+,«».؛،/:؟?@[\]^_`{|}~""",
 
     'epochs':8,
     # 'test_size':0.2,
